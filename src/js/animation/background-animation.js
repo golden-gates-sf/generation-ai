036767023ed0a18ce0d83152e583e8430c3d0d74
golden-gates-sf <redchenko.milana@gmail.com>
2024-05-22 13:25:09 +0300

    feat: add base main page with animated background + v2

---
+++ src/js/animation/background-animation.js
@@ -0,0 +1,13 @@
+VANTA.FOG({
+    el: "#your-element-selector",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0x8b37a4,
+    midtoneColor: 0xfff00,
+    baseColor: 0x0,
+    blurFactor: 0.66,
+    zoom: 0.50
+})
