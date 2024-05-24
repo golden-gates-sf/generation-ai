50e1fcb0372c2e7e9a22d1510e417b7ca4a9a396
golden-gates-sf <redchenko.milana@gmail.com>
2024-05-24 11:19:42 +0300

    fix: change typedText behaviour + heading color changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 import Typed from "typed.js";
 
+const typedStrings = ['идете в ногу со временем', 'делегируете задачи', 'не боитесь быть автоматизированными'];
+const pausedTypedStrings = typedStrings.map(str => '^1000 ' + str);
+
 const typeOptions = {
-    strings: ['идете в ногу со временем', 'делегируете задачи', 'не боитесь быть автоматизированными'],
+    strings: pausedTypedStrings,
     typeSpeed: 90,
-    backSpeed: 50,
+    backSpeed: 70,
     backDelay: 3000,
     loop: true,
     loopCount: Infinity
