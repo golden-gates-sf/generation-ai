8c5176de4003650cf697d9cc79d084153fcd4fbd
golden-gates-sf <redchenko.milana@gmail.com>
2024-05-27 13:16:02 +0300

    feat: add nav menu

--- src/js/index.js
+++ src/js/index.js
@@ -14,3 +14,25 @@ const typeOptions = {
 
 const typed = new Typed(".typed-text", typeOptions)
 
+const menuBtn = document.querySelector(".menu-btn");
+const navMenu = document.querySelector("nav");
+const menuBtnIcon = document.querySelector(".menu-btn-icon");
+const header = document.querySelector("header");
+
+console.log(menuBtnIcon.src.match('http://localhost:1234/menu-bold-icon-lb.svg'));
+console.log(menuBtnIcon.src)
+
+menuBtn.addEventListener("click", () => {
+    if (navMenu.style.visibility !== "visible") {
+        navMenu.style.visibility = "visible";
+        header.style.position = "fixed";
+        header.style.top = "0";
+        header.style.zIndex = "100";
+        // menuBtnIcon.src = "./src/img/icons/menu_icons/close-cross.svg";
+    } 
+    else {
+        navMenu.style.visibility = "hidden";
+        header.style.position = "static";
+        // menuBtnIcon.src = "/src/img/icons/menu_icons/menu-bold-icon-lb.svg";    
+    }
+})
