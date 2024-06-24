f9ed7d893ba4e554a76eb8bf0994feef8959b3ff
golden-gates-sf <redchenko.milana@gmail.com>
2024-06-24 13:35:28 +0300

    feat: add benefits long section

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import Typed from "typed.js";
 
+// Typed Text
+
 const typedStrings = [
   "идете в ногу со временем",
   "делегируете задачи",
@@ -29,6 +31,8 @@ const langDiv = document.getElementById("languages-container");
 const formOverlay = document.querySelector(".form-overlay");
 const registerBtn = document.querySelector(".register-btn");
 
+// NavBar
+
 menuBtn.addEventListener("click", () => {
   if (navMenu.style.visibility !== "visible") {
     navMenu.style.visibility = "visible";
@@ -43,13 +47,15 @@ menuBtn.addEventListener("click", () => {
   }
 });
 
+// Languages List
+
 langBtn.addEventListener("click", () => {
   if (langDropList.style.visibility === "visible") {
-    console.log('1');
+    console.log("1");
     langDropList.style.visibility = "hidden";
-    console.log('2');
+    console.log("2");
   } else {
-    console.log('0');
+    console.log("0");
     langDropList.style.visibility = "visible";
     langDropList.focus();
   }
@@ -66,34 +72,39 @@ langBtn.addEventListener("click", () => {
 
 langDropList.addEventListener("blur", () => {
   console.log("blur");
-  if (langDropList.style.visibility !== "hidden") langDropList.style.visibility = "hidden";
+  if (langDropList.style.visibility !== "hidden")
+    langDropList.style.visibility = "hidden";
 });
+
+// Register Form
 
 registerBtn.addEventListener("click", () => {
   const formInputContainer = document.getElementById("form-input-container");
   const slider = document.getElementById("slider");
   const switchCheckbox = document.querySelector("#switch-checkbox");
-  
+
   let switchMode = "Email";
   slider.innerText = "Telegram";
   slider.setAttribute("data-before", switchMode);
   switchCheckbox.checked = false;
 
-    formOverlay.style.visibility = "visible";
-    const closeFormBtn = document.getElementById("form-close-btn");
-    closeFormBtn.addEventListener("click", () => {
-      switchMode = 0;
-        formOverlay.style.visibility = "hidden";
-    });
+  formOverlay.style.visibility = "visible";
+  const closeFormBtn = document.getElementById("form-close-btn");
+  closeFormBtn.addEventListener("click", () => {
+    switchMode = 0;
+    formOverlay.style.visibility = "hidden";
+  });
 
-    slider.addEventListener("click", (e) => {
-      slider.setAttribute("data-before", switchMode);
+  slider.addEventListener("click", (e) => {
+    slider.setAttribute("data-before", switchMode);
 
-      if (switchMode === "Email") {
-        slider.style.justifyContent = "start";
-        slider.innerText = "Email";
-        formInputContainer.innerHTML = "";
-        formInputContainer.insertAdjacentHTML("afterbegin", `
+    if (switchMode === "Email") {
+      slider.style.justifyContent = "start";
+      slider.innerText = "Email";
+      formInputContainer.innerHTML = "";
+      formInputContainer.insertAdjacentHTML(
+        "afterbegin",
+        `
          <input
               type="text"
               name="Telegram Username"
@@ -101,15 +112,17 @@ registerBtn.addEventListener("click", () => {
               id="telegram-field"
               class="form-input"
           />
-          `)
-        switchMode = "Telegram";
-        slider.setAttribute("data-before", switchMode);
-      }
-      else {
-        slider.style.justifyContent = "end";
-        slider.innerText = "Telegram";
-        formInputContainer.innerHTML = "";
-        formInputContainer.insertAdjacentHTML("afterbegin", `
+          `
+      );
+      switchMode = "Telegram";
+      slider.setAttribute("data-before", switchMode);
+    } else {
+      slider.style.justifyContent = "end";
+      slider.innerText = "Telegram";
+      formInputContainer.innerHTML = "";
+      formInputContainer.insertAdjacentHTML(
+        "afterbegin",
+        `
           <input
               type="text"
               name="Username"
@@ -124,12 +137,139 @@ registerBtn.addEventListener("click", () => {
               id="email-field"
               class="form-input"
             />
-          `)
-        switchMode = "Email";
-        slider.setAttribute("data-before", switchMode);
-      }
-    })
-//   if (formOverlay.style.visibility !== "visible")
-//     formOverlay.classList.add("form--open");
-//   else formOverlay.classList.remove("form--open");
+          `
+      );
+      switchMode = "Email";
+      slider.setAttribute("data-before", switchMode);
+    }
+  });
+  //   if (formOverlay.style.visibility !== "visible")
+  //     formOverlay.classList.add("form--open");
+  //   else formOverlay.classList.remove("form--open");
+});
+
+// Benefits Cards Generation
+
+import benefitImg0 from "../img/benefits_section/benefits_0.webp";
+import benefitImg1 from "../img/benefits_section/benefits_1.webp";
+import benefitImg2 from "../img/benefits_section/benefits_2.webp";
+import benefitImg3 from "../img/benefits_section/benefits_3.webp";
+import benefitImg4 from "../img/benefits_section/benefits_4.webp";
+import benefitImg5 from "../img/benefits_section/benefits_5.webp";
+
+const benefitsCards = [
+  {
+    h1: "Job Security",
+    text1:
+      "Secure your future by mastering AI skills, safeguarding your position amidst rising automation",
+    h2: "Financial Stability",
+    text2:
+      "Improve your financial outlook with AI skills that open higher-paying job opportunities",
+    btnText: "Register to know how",
+    img: {
+      src: benefitImg0,
+      alt: "Happy Work Man",
+    },
+  },
+  {
+    h1: "Industry Relevance",
+    text1:
+      "Stay relevant in your industry by acquiring essential AI knowledge that employers demand",
+    h2: "Future-proof skills",
+    text2:
+      "Equip yourself with future-proof skills that ensure longevity in evolving job markets",
+    btnText: "Become an expert",
+    img: {
+      src: benefitImg1,
+      alt: "AI Engineering",
+    },
+  },
+  {
+    h1: "Anxiety Reduction",
+    text1:
+      "Reduce employment stress by being prepared for AI-driven job markets",
+    h2: "Economic Resilience",
+    text2:
+      "Build economic resilience by staying employable amidst technological disruption",
+    btnText: "Join us to learn more",
+    img: {
+      src: benefitImg2,
+      alt: "Happy Working People",
+    },
+  },
+  {
+    h1: "Hands-on Experience",
+    text1:
+      "Gain practical experience through projects and case studies aligned with industry needs",
+    h2: "Community Support",
+    text2:
+      "Join a supportive community of learners and professionals dedicated to AI excellence",
+    btnText: `I am ready, let's go`,
+    img: {
+      src: benefitImg3,
+      alt: "Developers Community",
+    },
+  },
+  {
+    h1: "Market Trends Insights",
+    text1:
+      "Leverage our research to understand and capitalize on upcoming AI job opportunities",
+    h2: "Resource Access",
+    text2:
+      "Access comprehensive AI resources and tools to enhance your learning experience",
+    btnText: "Get access now",
+    img: {
+      src: benefitImg4,
+      alt: "Observation",
+    },
+  },
+  {
+    h1: "Employment Assistance",
+    text1:
+      "Get personalized support to land lucrative positions requiring AI expertise",
+    h2: "Project Opportunities",
+    text2:
+      "Get matched with exciting AI projects that allow you to apply your newfound skills",
+    btnText: `Sounds good, I'm in`,
+    img: {
+      src: benefitImg5,
+      alt: "Employment Assistance",
+    },
+  },
+];
+
+const benefitsLongContainer = document.getElementById(
+  "benefits-long-container"
+);
+
+benefitsCards.forEach((el, i) => {
+  i % 2 === 0
+    ? benefitsLongContainer.insertAdjacentHTML(
+        "beforeend",
+        `
+      <div class="benefit-card">
+            <div class="benefit-card__text-box">
+              <h3>${el.h1}</h3>
+              <span>${el.text1}</span>
+              <h3>${el.h2}</h3>
+              <span>${el.text2}</span>
+              <button>${el.btnText}</button>
+            </div>
+            <img src="${el.img.src}" alt="${el.img.alt}">
+          </div>`
+      )
+    : benefitsLongContainer.insertAdjacentHTML(
+        "beforeend",
+        `
+      <div class="benefit-card">
+            <img src="${el.img.src}" alt="${el.img.alt}">
+            <div class="benefit-card__text-box">
+              <h3>${el.h1}</h3>
+              <span>${el.text1}</span>
+              <h3>${el.h2}</h3>
+              <span>${el.text2}</span>
+              <button>${el.btnText}</button>
+            </div>
+          </div>`
+      );
 });
